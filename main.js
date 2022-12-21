ded2dab6d3479fd94eeeb36e6d32e467b9d41b9e
HagarRam <hagaram11@gmail.com>
2022-12-21 16:38:19 +0200

    day7

--- main.js
+++ main.js
@@ -19,38 +19,40 @@ function statusMode(){
         }
     }
 
-    function checkPasswordStrength(password) {
-        if (password.length < 8) {
-          return "weak password";
+
+    //regex!
+    // function checkPasswordStrength(password) {
+    //     if (password.length < 8) {
+    //       return "weak password";
           
-        }
+    //     }
       
-        if (!password.match(/[a-z]/)) {
-          return "weak password";
-        }
+    //     if (!password.match(/[a-z]/)) {
+    //       return "weak password";
+    //     }
       
-        if (!password.match(/[A-Z]/)) {
-          return "weak password";
-        }
+    //     if (!password.match(/[A-Z]/)) {
+    //       return "weak password";
+    //     }
       
-        if (!password.match(/[0-9]/)) {
-          return "weak password";
-        }
+    //     if (!password.match(/[0-9]/)) {
+    //       return "weak password";
+    //     }
       
-        if (password.match(/\s/)) {
-          return "weak password";
-        }
+    //     if (password.match(/\s/)) {
+    //       return "weak password";
+    //     }
       
-        return "strong password";
-      }
-      function alertMode(){
-        alert(newPassword); 
-            if(newPassword.length < minNumberofChars || newPassword.length > maxNumberofChars){
-                return false;
-            }
-            if(!regularExpression.test(newPassword)) {
-                alert("password should contain atleast one number and one special character");
-                return false;
-            }
-      }
+    //     return "strong password";
+    //   }
+    //   function alertMode(){
+    //     alert(newPassword); 
+    //         if(newPassword.length < minNumberofChars || newPassword.length > maxNumberofChars){
+    //             return false;
+    //         }
+    //         if(!regularExpression.test(newPassword)) {
+    //             alert("password should contain atleast one number and one special character");
+    //             return false;
+    //         }
+    //   }
      
