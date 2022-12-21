c3c4f64d519bc547bb0c4b8e18d79a73d0fce8d2
HagarRam <hagaram11@gmail.com>
2022-12-21 16:27:35 +0200

    day 7

--- main.js
+++ main.js
@@ -6,3 +6,51 @@ function handleClick(){
 function delayHandleClick(){
     setTimeout(handleClick, 3000);
 }
+let element = document.body;
+let abc = false;
+function statusMode(){
+    if(!abc){
+        element.classList.add("dark");
+        abc = true;
+    }
+    else {
+        element.classList.remove("dark");
+        abc = false;
+        }
+    }
+
+    function checkPasswordStrength(password) {
+        if (password.length < 8) {
+          return "weak password";
+          
+        }
+      
+        if (!password.match(/[a-z]/)) {
+          return "weak password";
+        }
+      
+        if (!password.match(/[A-Z]/)) {
+          return "weak password";
+        }
+      
+        if (!password.match(/[0-9]/)) {
+          return "weak password";
+        }
+      
+        if (password.match(/\s/)) {
+          return "weak password";
+        }
+      
+        return "strong password";
+      }
+      function alertMode(){
+        alert(newPassword); 
+            if(newPassword.length < minNumberofChars || newPassword.length > maxNumberofChars){
+                return false;
+            }
+            if(!regularExpression.test(newPassword)) {
+                alert("password should contain atleast one number and one special character");
+                return false;
+            }
+      }
+     
